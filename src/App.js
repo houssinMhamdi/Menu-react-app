be13d9af0e86cae6e71815e67b63730def3f9270
Your Name <you@example.com>
2022-01-15 15:42:25 +0100

    filtering with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import Categories from './Categories';
+import Menu from './Menu';
+import items from './data'
+
+const allCategories =["all",...new Set(items.map((item)=>
+ item.category
+))] 
+
+console.log(allCategories);
 
 function App() {
+  const [menuItem,setMenuItem] = useState(items)
+  const [categories ,setCategories] = useState(allCategories)
+
+
+  const filterItems =(category) =>{
+    if (category === 'all') {
+      setMenuItem(items)
+      return
+    }
+
+    const newItem = items.filter((item)=>item.category === category)
+    setMenuItem(newItem)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+        <section className="menu section">
+            <div className="title">
+                <h2>our menu</h2>
+                <div className='underline'></div>
+            </div>
+             <Categories categories={categories} filterItems={filterItems} />
+            <Menu items={menuItem} />
+        </section>
+    </main>
   );
 }
 
---
+++ src/Categories.js
@@ -0,0 +1,14 @@
+export default function Categories({categories,filterItems}) {
+    return (
+        <div className="btn-container">
+            {categories.map((category,index)=>{
+                return(
+                    <button type="button" className="filter-btn"
+                    onClick={()=>filterItems(category)}
+                    key={index}
+                    >{category}</button>
+                )
+            })}
+        </div>
+    )
+}
